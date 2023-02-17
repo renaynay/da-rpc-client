63eb2e94792d0f482fd0eb2bcf492af1dfeabe50
rene <41963722+renaynay@users.noreply.github.com>
2023-02-17 20:49:04 +0100

    support two clients filling blocks simultaneously, gets block size to 256

--- rpc.go
+++ rpc.go
@@ -5,36 +5,56 @@ import (
 	"cosmossdk.io/math"
 	"crypto/rand"
 	"fmt"
+	"github.com/celestiaorg/celestia-node/api/rpc/client"
 	"os"
 	"time"
-
-	"github.com/celestiaorg/celestia-node/api/rpc/client"
 )
 
 func main() {
 	addr := os.Args[1]
 	token := os.Args[2]
+	addr2 := os.Args[3]
+	token2 := os.Args[4]
 
-	fillBlocks(token, addr)
+	fillBlocks(token, addr, token2, addr2)
 }
 
-func fillBlocks(token, addr string) {
-	client, err := client.NewClient(context.Background(), addr, token)
+// 256 size block
+func fillBlocks(token, addr, token2, addr2 string) {
+	client1, err := client.NewClient(context.Background(), addr, token)
+	if err != nil {
+		panic(err)
+	}
+	client2, err := client.NewClient(context.Background(), addr2, token2)
 	if err != nil {
 		panic(err)
 	}
 
 	for {
-		nID := make([]byte, 8)
-		rand.Read(nID)
-		data := make([]byte, 1000000) // todo configure this
-		rand.Read(data)
-		resp, err := client.State.SubmitPayForBlob(context.Background(), nID, data, math.NewInt(50000), 9000000)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println("resp code: ", resp.Code)
-		fmt.Println("resp height: ", resp.Height, "\n")
+		go func() {
+			nID := make([]byte, 8)
+			rand.Read(nID)
+			data := make([]byte, 1005000) // todo configure this
+			rand.Read(data)
+			resp, err := client1.State.SubmitPayForBlob(context.Background(), nID, data, math.NewInt(500000), 90000000)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println("client1: resp code: ", resp.Code)
+			fmt.Println("client1: resp height: ", resp.Height, "\n")
+		}()
+		go func() {
+			nID := make([]byte, 8)
+			rand.Read(nID)
+			data := make([]byte, 1005000) // todo configure this
+			rand.Read(data)
+			resp, err := client2.State.SubmitPayForBlob(context.Background(), nID, data, math.NewInt(500000), 90000000)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println("client2: resp code: ", resp.Code)
+			fmt.Println("client2: resp height: ", resp.Height, "\n")
+		}()
 		time.Sleep(15 * time.Second)
 	}
 }
